945ea0e74d9c55e2d37d00ee86ec553a78b931b5
Ángel Carias <69056344+aescarias@users.noreply.github.com>
2024-05-08 21:51:32 -0600

    feat(parser): add new PdfDocument
    This lays down the foundations for a high-level PDF interface.

--- src/pdfnaut/__init__.py
+++ src/pdfnaut/__init__.py
@@ -4,8 +4,9 @@ PDFnaut is a Python library for reading and writing PDFs at a low level.
 
 from .parsers import PdfParser
 from .serializer import PdfSerializer
+from .document import PdfDocument
 
-__all__ = ("PdfParser", "PdfSerializer")
+__all__ = ("PdfParser", "PdfSerializer", "PdfDocument")
 
 __name__ = "pdfnaut"
 __version__ = "0.2.0"
