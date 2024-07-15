0b095be5b81f5c09d3266a7c316f3190b7799dab
Ángel Carias <69056344+aescarias@users.noreply.github.com>
2024-07-15 16:19:25 -0600

    feat: 0.4.0
    A few structure changes, renames, and fixes (as always!)

--- src/pdfnaut/__init__.py
+++ src/pdfnaut/__init__.py
@@ -9,6 +9,6 @@ from .document import PdfDocument
 __all__ = ("PdfParser", "PdfTokenizer", "PdfSerializer", "PdfDocument")
 
 __name__ = "pdfnaut"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __description__ = "Explore PDFs with ease"
 __license__ = "Apache 2.0"
