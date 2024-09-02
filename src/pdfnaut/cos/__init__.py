ed3f81ada4b389e4483c3cdcb6addb661e573e8e
Ángel Carias <69056344+aescarias@users.noreply.github.com>
2024-09-02 10:37:17 -0600

    feat(parser): add PDFDocEncoding
    Adds PDFDocEncoding as a new codec (pdfdoc or pdfdoc_naut). Defined in 'Annex D.3. PDFDocEncoding character set' of IS0 32000-2.

--- src/pdfnaut/cos/__init__.py
+++ src/pdfnaut/cos/__init__.py
@@ -2,4 +2,7 @@ from .parser import PdfParser
 from .serializer import PdfSerializer
 from .tokenizer import ContentStreamIterator, PdfTokenizer
 
+# Encodings registered
+from .encodings import pdfdoc as pdfdoc
+
 __all__ = ("PdfParser", "PdfTokenizer", "PdfSerializer", "ContentStreamIterator")
